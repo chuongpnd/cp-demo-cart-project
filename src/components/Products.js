0ae9debb197d3edd83fe21c72bd407d0d6e99e60
chuongpnd <macbookpro@chuongpnd.local>
2020-02-19 10:17:34 +0700

    show product on homepage

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import Product from './Product';
+import { connect } from 'react-redux';
 
 class Products extends React.Component {
     render() {
+        var { productList } = this.props;
+        var result = productList.length > 0 ?
+                    productList.map((product, index) => {
+                        return <Product key={index} product={product} />
+                    }) : []
         return (
             <section className="section">
                 <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
                 <div className="row">
-                    <Product />
-                    <Product />
-                    <Product />
+                    { result }
                 </div>
             </section>
         );
     }
 }
 
-export default Products;
+const mapStateToProps = state => {
+    return {
+        productList: state.productsReducer
+    }
+}
+
+export default connect(mapStateToProps, null)(Products);
